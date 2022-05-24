4781bd7026f04e394be31c5205f1d3768a4641c4
beltre.wilton <beltre.wilton@gmail.com>
2022-05-24 11:44:27 -0400

    new finger labs

--- source/case_study_3_1/lab.3.3.4.py
+++ source/case_study_3_1/lab.3.3.4.py
@@ -13,7 +13,7 @@ def find_nearest_neighborns(p, points, k=5):
     """
     distances = np.zeros(points.shape[0])
     for i in range(len(distances)):
-        distances[i] = np.sqrt(np.power(p - points[i], 2))
+        distances[i] = np.sum(np.sqrt(np.power(p - points[i], 2)))
     ind = np.argsort(distances)
     return ind[0:k]
 
@@ -30,7 +30,7 @@ def generate_synth_data(n=50):
     :param n:
     :return:
     """
-    points = np.concatenate((stats.norm(0,1).rvs((n,2)), stats.norm(1,1).rvs((n,2))), axis=0)
+    points = np.concatenate((stats.norm(0, 1).rvs((n, 2)), stats.norm(1, 1).rvs((n, 2))), axis=0)
     outcomes = np.concatenate((np.repeat(0, n), np.repeat(1, n)), axis=0)
     return points, outcomes
 
@@ -48,4 +48,46 @@ def plot_synth_data(points, outcomes, n=50):
     plt.show()
 
 
-print()
+def plot_prediction_grid (xx, yy, prediction_grid):
+    """ Plot KNN predictions for every point on the grid."""
+    from matplotlib.colors import ListedColormap
+    background_colormap = ListedColormap (["hotpink","lightskyblue", "yellowgreen"])
+    observation_colormap = ListedColormap (["red","blue","green"])
+    plt.figure(figsize =(10,10))
+    plt.pcolormesh(xx, yy, prediction_grid, cmap = background_colormap, alpha = 0.5)
+    plt.scatter(xx[:,0], yy [:,1], c = outcomes, cmap = observation_colormap, s = 50)
+    plt.xlabel('Variable 1'); plt.ylabel('Variable 2')
+    plt.xticks(()); plt.yticks(())
+    plt.xlim (np.min(xx), np.max(xx))
+    plt.ylim (np.min(yy), np.max(yy))
+    plt.show()
+
+
+def make_prediction_grid(predictors, outcomes, limits, h, k):
+    """
+    Classsify each point in the prediction grid.
+    :param predictors:
+    :param outcomes:
+    :param limits:
+    :param h:
+    :param k:
+    :return:
+    """
+    (x_min, x_max, y_min, y_max) = limits
+    xs = np.arange(x_min, x_max, h)
+    ys = np.arange(y_min, y_max, h)
+    xx, yy = np.meshgrid(xs, ys)
+
+    prediction_grid = np.zeros(xx.shape, dtype=int)
+    for i, x in enumerate(xs):
+        for j, y in enumerate(ys):
+            p = np.array([x, y])
+            prediction_grid[j, i] = knn_predict(p, predictors, outcomes, k)
+
+    return xx, yy, prediction_grid
+
+points, outcomes = generate_synth_data()
+
+k=5; limits = (-3, 4, -3, 4); h = .1
+(xx, yy, prediction_grid) = make_prediction_grid(points, outcomes, limits, h=h, k=k)
+plot_prediction_grid(xx, yy, prediction_grid)
